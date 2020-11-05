00fa95964bc7f1a38e97425d1aa7e696d359b7f0
saroar <saroar9@gmail.com>
2020-11-05 22:44:40 +0300

    swith coding sytle update remove 4 space and use 2 space

--- Sources/AddaAPIGatewayModels/Models/User.swift
+++ Sources/AddaAPIGatewayModels/Models/User.swift
@@ -11,74 +11,79 @@ import Fluent
 import FluentMongoDriver
 
 public final class User: Model, Content, Hashable {
-
-    public static var schema = "users"
-
-    public init() {}
-    
-    public init(id: ObjectId? = nil ,phoneNumber: String) {
-        self.id = id
-        self.phoneNumber = phoneNumber
-        self.firstName = nil
-        self.lastName = nil
-    }
-    
-    @ID(custom: "id") public var id: ObjectId?
-    @Field(key: "phoneNumber") public var phoneNumber: String
-    
-    @OptionalField(key: "firstName") public var firstName: String?
-    @OptionalField(key: "lastName") public var lastName: String?
-    @OptionalField(key: "email") public var email: String?
-    
-    @Children(for: \.$owner) public var event: [Event]
-    @Children(for: \.$sender) public var sender: [Message]
-    @Children(for: \.$recipient) public var recipient: [Message]
-
-    @Siblings(through: UserConversation.self, from: \.$member, to: \.$conversation)
-    public var membersConversaions: [Conversation]
-
-    @Siblings(through: UserConversation.self, from: \.$admin, to: \.$conversation)
-    public var adminsConversations: [Conversation]
-
-    @Timestamp(key: "createdAt", on: .create) public var createdAt: Date?
-    @Timestamp(key: "updatedAt", on: .update) public var updatedAt: Date?
-    @Timestamp(key: "deletedAt", on: .delete) public var deletedAt: Date?
-    
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-    
-    public static func == (lhs: User, rhs: User) -> Bool {
-        lhs.id == rhs.id
-    }
+  
+  public static var schema = "users"
+  
+  public init() {}
+  
+  public init(id: ObjectId? = nil ,phoneNumber: String) {
+    self.id = id
+    self.phoneNumber = phoneNumber
+    self.firstName = nil
+    self.lastName = nil
+  }
+  
+  @ID(custom: "id") public var id: ObjectId?
+  @Field(key: "phoneNumber") public var phoneNumber: String
+  
+  @OptionalField(key: "firstName") public var firstName: String?
+  @OptionalField(key: "lastName") public var lastName: String?
+  @OptionalField(key: "email") public var email: String?
+  
+  @Children(for: \.$owner) public var event: [Event]
+  @Children(for: \.$sender) public var sender: [Message]
+  @Children(for: \.$recipient) public var recipient: [Message]
+  
+  @Siblings(through: UserConversation.self, from: \.$member, to: \.$conversation)
+  public var membersConversaions: [Conversation]
+  
+  @Siblings(through: UserConversation.self, from: \.$admin, to: \.$conversation)
+  public var adminsConversations: [Conversation]
+  
+  @Timestamp(key: "createdAt", on: .create) public var createdAt: Date?
+  @Timestamp(key: "updatedAt", on: .update) public var updatedAt: Date?
+  @Timestamp(key: "deletedAt", on: .delete) public var deletedAt: Date?
+  
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(id)
+  }
+  
+  public static func == (lhs: User, rhs: User) -> Bool {
+    lhs.id == rhs.id
+  }
 }
 
 extension User {
-    public var response: Response {
-        .init(self)
+  
+  public var amConversations: [Conversation] {
+    return self.adminsConversations + self.membersConversaions
+  }
+  
+  public var response: Response {
+    .init(self)
+  }
+  
+  public struct Response: Content {
+    
+    public init(_ user: User) {
+      self.id = user.id
+      self.firstName = user.firstName
+      self.lastName = user.lastName
+      self.email = user.email
+      self.phoneNumber = user.phoneNumber
+      
+      self.createdAt = user.createdAt
+      self.updatedAt = user.updatedAt
+      self.deletedAt = user.deletedAt
     }
     
-    public struct Response: Content {
-        
-        public init(_ user: User) {
-            self.id = user.id
-            self.firstName = user.firstName
-            self.lastName = user.lastName
-            self.email = user.email
-            self.phoneNumber = user.phoneNumber
-            
-            self.createdAt = user.createdAt
-            self.updatedAt = user.updatedAt
-            self.deletedAt = user.deletedAt
-        }
-        
-        public var id: ObjectId?
-        public var firstName, lastName, email: String?
-        public var phoneNumber: String
-        public var adminsConversations: [Conversation]?
-        public var membersConversaions: [Conversation]?
-        public var createdAt, updatedAt, deletedAt: Date?
-    }
+    public var id: ObjectId?
+    public var firstName, lastName, email: String?
+    public var phoneNumber: String
+    public var adminsConversations: [Conversation]?
+    public var membersConversaions: [Conversation]?
+    public var createdAt, updatedAt, deletedAt: Date?
+  }
 }
 
 
