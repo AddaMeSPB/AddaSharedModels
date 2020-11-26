c26a50793fd35ec57b2feec406d8279194cd2a4b
saroar <saroar9@gmail.com>
2020-11-26 20:31:17 +0300

    remove EventPlave and move place info to Event model

--- Sources/AddaAPIGatewayModels/Models/Event.swift
+++ Sources/AddaAPIGatewayModels/Models/Event.swift
@@ -15,28 +15,70 @@ public final class Event: Model, Content {
   
   public init() {}
   
-  public init(id: ObjectId? = nil, name: String, imageUrl: String? = nil, duration: Int, categories: String, isActive: Bool, ownerId: User.IDValue, conversationId: Conversation.IDValue) {
+  public init(
+    id: ObjectId? = nil, name: String, details: String? = nil,
+    imageUrl: String? = nil, duration: Int, categories: String,
+    isActive: Bool, addressName: String,
+    geoType: GeoType, coordinates: [Double],
+    sponsored: Bool? = false, overlay: Bool? = false,
+    ownerId: User.IDValue, conversationsId: Conversation.IDValue
+  ) {
     self.id = id
     self.name = name
+    self.details = details
     self.imageUrl = imageUrl
     self.duration = duration
     self.categories = categories
     self.isActive = isActive
+    
+    // Place information
+    self.addressName = addressName
+    self.type = geoType
+    self.coordinates = coordinates
+    self.sponsored = sponsored
+    self.overlay = overlay
+    
     self.$owner.id = ownerId
-    self.$conversation.id = conversationId
+    self.$conversation.id = conversationsId
+  }
+  
+  public init(content: CUEvent, ownerID: ObjectId, conversationsID: ObjectId) {
+    self.name = content.name
+    self.details = content.details
+    self.imageUrl = content.imageUrl
+    self.duration = content.duration
+    self.categories = content.categories
+    self.isActive = content.isActive
+    
+    // Place information
+    self.addressName = content.addressName
+    self.type = content.type
+    self.coordinates = content.coordinates
+    self.sponsored = content.sponsored
+    self.overlay = content.overlay
+    
+    self.$owner.id = ownerID
+    self.$conversation.id = conversationsID
+
   }
   
   @ID(custom: "id") public var id: ObjectId?
   @Field(key: "name") public var name: String
+  @OptionalField(key: "details") public var details: String?
   @OptionalField(key: "imageUrl") public var imageUrl: String?
   @Field(key: "duration") public var duration: Int
   @Field(key: "categories") public var categories: String
   @Field(key: "isActive") public var isActive: Bool
   
-  @Children(for: \.$event) public var eventPlaces: [EventPlace]
-  
+  // Place information
+  @Field(key: "addressName") public var addressName: String
+  @Field(key: "type") public var type: GeoType
+  @Field(key: "coordinates") public var coordinates: [Double]
+  @OptionalField(key: "sponsored") public var sponsored: Bool?
+  @OptionalField(key: "overlay") public var overlay: Bool?
+
   @Parent(key: "ownerId") public var owner: User
-  @Parent(key: "conversationId") public var conversation: Conversation
+  @Parent(key: "conversationsId") public var conversation: Conversation
   
   @Timestamp(key: "createdAt", on: .create) public var createdAt: Date?
   @Timestamp(key: "updatedAt", on: .update) public var updatedAt: Date?
@@ -51,42 +93,152 @@ extension Event {
   }
   
   public struct Item: Content {
+    public init(_id: ObjectId? = nil, name: String, details: String? = nil, imageUrl: String? = nil, duration: Int, isActive: Bool, conversationsId: ObjectId, categories: String, addressName: String, sponsored: Bool? = nil, overlay: Bool? = nil, type: GeoType, coordinates: [Double], updatedAt: Date?, createdAt: Date?, deletedAt: Date?) {
+      self._id = _id
+      self.name = name
+      self.details = details
+      self.imageUrl = imageUrl
+      self.duration = duration
+      self.isActive = isActive
+      self.conversationsId = conversationsId
+      self.categories = categories
+      self.addressName = addressName
+      self.sponsored = sponsored
+      self.overlay = overlay
+      self.type = type
+      self.coordinates = coordinates
+      self.updatedAt = updatedAt
+      self.createdAt = createdAt
+      self.deletedAt = deletedAt
+    }
+    
+    public var recreateEventWithSwapCoordinatesForMongoDB: Event.Item {
+      .init(_id: _id, name: name, details: details, imageUrl: imageUrl, duration: duration, isActive: isActive, conversationsId: conversationsId, categories: categories, addressName: addressName, sponsored: sponsored, overlay: overlay, type: type, coordinates: swapCoordinatesForMongoDB(), updatedAt: updatedAt, createdAt: createdAt, deletedAt: deletedAt)
+    }
+    
     public init(_ event: Event) {
-      self.id = event.id
+      self._id = event.id
       self.name = event.name
+      self.details = event.details
       self.imageUrl = event.imageUrl
       self.duration = event.duration
       self.categories = event.categories
-      self.owner = event.owner
-      self.conversation = event.conversation
-      self.eventPlaces = event.eventPlaces.map { $0.response }
       self.isActive = event.isActive
+      self.conversationsId = event.$conversation.id
+      
+      // Place information
+      self.addressName = event.addressName
+      self.type = event.type
+      self.coordinates = event.coordinates
+      self.sponsored = event.sponsored
+      self.overlay = event.overlay
+      
+      //db.events.updateMany({}, [{ $set: { addressName: "", details: "if you want you explain about your event" type: "Point", coordinates: [29.873706166262373, 60.26134045287572], sponsored: false, overlay: false } }])
+            
       self.createdAt = event.createdAt
       self.updatedAt = event.updatedAt
       self.deletedAt = event.deletedAt
     }
     
-    
-    public var id: ObjectId?
+    public var _id: ObjectId?
     public var name: String
+    public var details: String?
     public var imageUrl: String?
     public var duration: Int
-    public var categories: String
-    public var eventPlaces: [EventPlace.Item]
-    public var owner: User?
-    public var conversation: Conversation?
     public var isActive: Bool
+    public var categories: String
+    public var conversationsId: ObjectId
+    
+    // Place Information
+    public var addressName: String
+    public var sponsored: Bool?
+    public var overlay: Bool?
+    public var type: GeoType
+    public var coordinates: [Double]
+    
     public let updatedAt, createdAt, deletedAt: Date?
+    
+    public func swapCoordinatesForMongoDB() -> [Double] {
+      return [coordinates[1], coordinates[0]]
+    }
   }
+  
+ 
+}
+
+
+public struct EventPage: Content {
+    /// The page's items. Usually models.
+  public let items: [Event.Item]
+
+    /// Metadata containing information about current page, items per page, and total items.
+    public let metadata: PageMetadata
+
+    /// Creates a new `Page`.
+    public init(items: [Event.Item], metadata: PageMetadata) {
+        self.items = items
+        self.metadata = metadata
+    }
+}
+
+public struct EventPageRequest: Decodable {
+
+    public let page: Int
+    public let per: Int
+    public let lat: Double
+    public let long: Double
+    public let distance: Int
+
+    enum CodingKeys: String, CodingKey {
+        case page = "page"
+        case per = "per"
+        case lat = "lat"
+        case long = "long"
+        case distance = "distance"
+    }
+
+    /// `Decodable` conformance.
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.page = try container.decodeIfPresent(Int.self, forKey: .page) ?? 1
+        self.per = try container.decodeIfPresent(Int.self, forKey: .per) ?? 10
+        self.lat = try container.decodeIfPresent(Double.self, forKey: .lat) ?? 59.9311
+        self.long = try container.decodeIfPresent(Double.self, forKey: .long) ?? 30.3609
+        self.distance = try container.decodeIfPresent(Int.self, forKey: .distance) ?? 250
+    }
+
+    public init(page: Int, per: Int, lat: Double, long: Double, distance: Int) {
+        self.page = page
+        self.per = per
+        self.lat = lat
+        self.long = long
+        self.distance = distance
+    }
+
+    var start: Int {
+        (self.page - 1) * self.per
+    }
+
+    var end: Int {
+        self.page * self.per
+    }
 }
 
 public struct CUEvent: Content {
   public var name: String
+  public var details: String?
   public var imageUrl: String?
   public var duration: Int
   public var categories: String
-  public var ownerId: ObjectId?
-  public var conversationId: ObjectId?
   public var isActive: Bool
+  
+  public var addressName: String
+  public var type: GeoType
+  public var coordinates: [Double]
+  public var sponsored: Bool?
+  public var overlay: Bool?
+  
+  public var ownerId: ObjectId?
+  public var conversationsId: ObjectId?
 }
 
